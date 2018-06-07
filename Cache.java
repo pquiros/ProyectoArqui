4e82d8ffa7f50dce464b125f138d5110a4d06025
pquiros <pquiros00@gmail.com>
2018-06-06 22:31:26 -0600

    Initial commit

---
+++ Cache.java
@@ -0,0 +1,26 @@
+public class Cache {
+
+    public CPU cpu;
+    char estados[];
+    int etiquetas[];
+
+    public Cache() {
+        cpu = new CPU();
+    }
+
+    public void insertFromMemory() {
+
+    }
+
+    public boolean checkCache() {
+        return;
+    }
+
+    public getFromCache() {
+
+    }
+
+    public void loadFromMemory() {
+
+    }
+}
