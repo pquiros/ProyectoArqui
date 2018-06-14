3d2e198516cf314a7dfc6152e299add8c8e929c8
KevinWV <kevin.waltam@gmail.com>
2018-06-14 12:18:26 -0600

    hay voy

--- Nucleo.java
+++ Nucleo.java
@@ -1,4 +1,6 @@
-public class Nucleo {
+
+
+public class Nucleo implements runnable {
 
     CPU cpu;
     int registrosHilo0[];
@@ -7,18 +9,40 @@ public class Nucleo {
     int id;
     int idHilillo;
 
+    HijoSuicida hijoSuicida;
+
     public Nucleo(int tipo) {
         registrosHilo0 = new int[32];
 
         if (tipo == 0) {
             registrosHilo1 = new int[32];
             pc = new int[2];
+            hijoSuicida = new HijoSuicida();
         }else {
             pc = new int[1];
         }
 
         id = tipo;
         idHilillo = 0;
+    }
 
+    void cargarHilillo(){}
+    void fetch(){}
+    void ejecutarI(String instruccion){}
+    int convertDirBloque(int dir){return 0;}
+    void LW(){}
+    void SW(){}
+    void falloDeCache(){
+        if(id==0){
+            hijoSuicida.run();
+        }
+    }
+
+    void run(){
+
+    }
+    private class HijoSuicida implements runnable{
+        public HijoSuicida(){}
+        public run(){}
     }
 }
