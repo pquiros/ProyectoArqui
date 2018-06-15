294f9b16e34ec910d84a0c5a7a029f0f8576d81c
KevinWV <kevin.waltam@gmail.com>
2018-06-14 20:09:51 -0600

    8===D

--- CPU.java
+++ CPU.java
@@ -1,4 +1,12 @@
+import  java.util.LinkedList;
+
 public class CPU {
+    LinkedList<Contextos> contextos;
+    LinkedList<BufferedReader> hilillos;
+    int[] RAMD;
+    int[] RAMI;
+    int Quatum;
+
 
     public static void main(String [] args) {
 
--- Nucleo.java
+++ Nucleo.java
@@ -1,6 +1,6 @@
 
 
-public class Nucleo implements runnable {
+public class Nucleo implements Runnable {
 
     CPU cpu;
     int registrosHilo0[];
@@ -8,12 +8,15 @@ public class Nucleo implements runnable {
     int pc[];
     int id;
     int idHilillo;
+    Cache cacheI;
+    Cache cacheD;
 
     HijoSuicida hijoSuicida;
 
-    public Nucleo(int tipo) {
+    public Nucleo(int tipo, Cache cD,Cache  cI) {
+        cacheD= cD;
+        cacheI= cI;
         registrosHilo0 = new int[32];
-
         if (tipo == 0) {
             registrosHilo1 = new int[32];
             pc = new int[2];
@@ -21,27 +24,31 @@ public class Nucleo implements runnable {
         }else {
             pc = new int[1];
         }
-
         id = tipo;
         idHilillo = 0;
     }
 
     void cargarHilillo(){}
     void fetch(){}
-    void ejecutarI(String instruccion){}
+    void ejecutarI(String instruccion){
+
+        switch(2){}
+    }
     int convertDirBloque(int dir){return 0;}
     void LW(){}
     void SW(){}
     void falloDeCache(){
         if(id==0){
-            hijoSuicida.run();
+            hijoSuicida = new HijoSuicida();
+            Threat hilo= new Threat(hijoSuicida);
+            hilo.start();
         }
     }
 
     void run(){
 
     }
-    private class HijoSuicida implements runnable{
+    private class HijoSuicida implements Runnable{
         public HijoSuicida(){}
         public run(){}
     }
